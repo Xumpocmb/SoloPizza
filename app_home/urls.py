8f209b438bd987b6b98cfdb4d893b848b0d69c9a
xumpocmb <youarenear26@gmail.com>
2025-07-17 23:24:46 +0300

    discounts

--- app_home/urls.py
+++ app_home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from app_home.views import home_page, select_branch
+from app_home.views import home_page, select_branch, discounts_view
 
 app_name = 'app_home'
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('select_branch/', select_branch, name='select_branch'),
+    path('discounts/', discounts_view, name='discounts'),
 ]
--- app_home/views.py
+++ app_home/views.py
@@ -1,16 +1,27 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from app_catalog.models import Product
 
 
 def home_page(request):
-    return render(request, 'app_home/home.html')
+    return render(request, "app_home/home.html")
 
 
+@require_POST
 def select_branch(request):
-    branch_id = request.GET.get('branch_id')
+    branch_id = request.POST.get("branch_id")
+    print(branch_id)
     if branch_id:
-        request.session['selected_branch_id'] = branch_id
-        messages.success(request, 'Филиал изменен!', extra_tags='success')
+        request.session["selected_branch_id"] = branch_id
+        messages.success(request, "Филиал изменен!", extra_tags="success")
     else:
-        messages.error(request, 'Ошибка: филиал не выбран!', extra_tags='error')
-    return redirect(request.META.get('HTTP_REFERER'))
+        messages.error(request, "Ошибка: филиал не выбран!", extra_tags="error")
+    return redirect(request.META.get("HTTP_REFERER"))
+
+
+def discounts_view(request):
+    pizza_weekly = Product.objects.filter(category__slug="picca", is_active=True, is_weekly_special=True).first()
+    context = {"pizza_weekly": pizza_weekly}
+    return render(request, "app_home/discounts.html", context=context)
