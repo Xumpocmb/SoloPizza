2c471cc7ea10bdd9187f3cb0662771408c08a4ff
xumpocmb <youarenear26@gmail.com>
2025-08-07 15:54:02 +0300

    seo opt

---
+++ SoloPizza/sitemap.py
@@ -0,0 +1,62 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from app_catalog.models import Category, Product
+from app_home.models import Vacancy
+from app_reviews.models import Review
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.5
+    changefreq = 'daily'
+
+    def items(self):
+        return ['app_home:home', 'app_home:contacts', 'app_home:feedback', 
+                'app_home:vacancy_list', 'app_catalog:catalog', 
+                'app_reviews:review_list']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class CategorySitemap(Sitemap):
+    priority = 0.7
+    changefreq = 'weekly'
+
+    def items(self):
+        return Category.objects.filter(is_active=True)
+
+    def location(self, obj):
+        return obj.get_absolute_url()
+
+
+class ProductSitemap(Sitemap):
+    priority = 0.8
+    changefreq = 'daily'
+
+    def items(self):
+        return Product.objects.filter(is_active=True)
+
+    def location(self, obj):
+        return obj.get_absolute_url()
+
+
+class VacancySitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'weekly'
+
+    def items(self):
+        return Vacancy.objects.filter(is_active=True)
+
+    def location(self, obj):
+        return reverse('app_home:vacancy_detail', kwargs={'pk': obj.pk})
+
+
+class ReviewSitemap(Sitemap):
+    priority = 0.4
+    changefreq = 'weekly'
+
+    def items(self):
+        return Review.objects.filter(status='approved')
+
+    def location(self, obj):
+        return reverse('app_reviews:review_detail', kwargs={'pk': obj.pk})
--- SoloPizza/urls.py
+++ SoloPizza/urls.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .sitemap import StaticViewSitemap, CategorySitemap, ProductSitemap, VacancySitemap, ReviewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'categories': CategorySitemap,
+    'products': ProductSitemap,
+    'vacancies': VacancySitemap,
+    'reviews': ReviewSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,4 +26,11 @@ urlpatterns = [
     path("cart/", include("app_cart.urls")),
     path("order/", include("app_order.urls")),
     path("reviews/", include("app_reviews.urls")),
+    
+    # SEO URLs
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
