a7de6172bad0677993afc25aabc0522d4cffe2c0
xumpocmb <youarenear26@gmail.com>
2025-08-08 15:34:27 +0300

    .

--- restart_services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Очистка Redis
-docker exec -it solopizza-redis-1 redis-cli flushall
-echo "Redis cache cleared"
-
-# Перезапуск веб-сервера
-docker restart solopizza-web-1
-echo "Web server restarted"
