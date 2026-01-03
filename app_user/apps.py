326e19ad39a84fed7148e871f9443b4d7ae5dd28
xumpocmb <youarenear26@gmail.com>
2026-01-04 00:05:01 +0300

    random users

--- app_user/apps.py
+++ app_user/apps.py
@@ -5,3 +5,6 @@ class AppUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_user'
     verbose_name = 'Пользователи'
+
+    def ready(self):
+        import app_user.models
